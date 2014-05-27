1da8561766ec65d8bfac29337c209a60048bbe40
Frederic Bour <frederic.bour@lakaban.net>
2014-05-27 17:18:42 +0100

    Fix equality equations

--- typing/ctype.mli
+++ typing/ctype.mli
@@ -283,3 +283,4 @@ val package_subtype :
 val modtype_of_package : (Env.t -> Location.t -> Path.t -> Longident.t list ->
                           type_expr list -> module_type) ref
 
+val new_declaration : (int * int) option -> type_expr option -> type_declaration
