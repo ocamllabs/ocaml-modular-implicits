3cd757bfa4806c0f18c4fe460f895fcbf497120e
Jeremy Yallop <yallop@gmail.com>
2015-08-18 09:25:14 +0100

    Travis tests: use Camlp4 branch with modular implicits support.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,11 +3,23 @@ i386)
   ./configure
   make world.opt
   sudo make install
-  (cd testsuite && make all)
-  git clone git://github.com/ocaml/camlp4 -b 4.02
-  (cd camlp4 && ./configure && make && sudo make install)
+
+  cd testsuite && make all
+
+  git clone git://github.com/ocamllabs/camlp4
+  (cd camlp4                              \
+   && git checkout modular-implicits-4.02 \
+   && ./configure                         \
+   && make                                \
+   && sudo make install)
+
   git clone git://github.com/ocaml/opam
-  (cd opam && ./configure && make lib-ext && make && sudo make install)
+  (cd opam                                \
+   && ./configure                         \
+   && make lib-ext                        \
+   && make                                \
+   && sudo make install)
+
   opam init -y -a git://github.com/ocaml/opam-repository
   opam install -y utop
   ;;
